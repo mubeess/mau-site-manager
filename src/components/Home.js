2a933afbfd31aa66627109a4aa54029d88cfa195
mubeess <mubarakibrahim2015@gmail.com>
2022-03-17 23:39:29 +0100

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { message, Skeleton, Upload } from 'antd';
 import { Avatar, Button,Input } from 'antd';
-import {PlusCircleOutlined, PlusOutlined, UploadOutlined} from '@ant-design/icons'
+import {DeleteOutlined, PlusCircleOutlined, PlusOutlined, UploadOutlined} from '@ant-design/icons'
 import Modal from 'antd/lib/modal/Modal';
 
 export default function Home() {
@@ -16,35 +16,12 @@ export default function Home() {
 
     const props = {
         name: 'profile_pic',
-        action: `https://modibbo-adama.herokuapp.com/admin/upload-an-image?eventId=${uploaOptions}&eventName=mainEvents&activity=homepage`,
+        action: `https://new-modibbo-adama.herokuapp.com/admin/upload-an-image?eventId=${uploaOptions}&eventName=mainEvents&activity=homepage`,
         headers: {
           authorization: 'authorization-text',
         },
         method:'put',
-        beforeUpload(){
-            const myObj={
-                evnt:{
-                    header:title,
-                    subHeader:header,
-                    description:desc
-                },
-                homeEventType:'mainEvents'
-            }
-            fetch('https://modibbo-adama.herokuapp.com/admin/create-home-event',{
-                method:'PUT',
-                headers:{
-                  "Content-Type":'application/json'
-                },
-                body:JSON.stringify(myObj)
-              })
-              .then(res=>{
-                  res.json()
-                  .then(data=>{
-                     setUploadOpt(data.newlyEvent.evntId)
-                  })
-              })
-        }
-        ,
+  
         onChange(info) {
           if (info.file.status !== 'uploading') {
             console.log(info.file, info.fileList);
@@ -56,6 +33,35 @@ export default function Home() {
             setDesc('')
             loadData()
             handleCancel()
+          } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+          }
+        },
+        
+      };
+
+
+
+
+
+      const props2 = {
+        name: 'profile_pic',
+        action: `https://new-modibbo-adama.herokuapp.com/admin/upload-an-image?eventId=${homeData.length>0?homeData[0].vc.evntId:''}&activity=vc`,
+        headers: {
+          authorization: 'authorization-text',
+        },
+        method:'put',
+  
+        onChange(info) {
+          if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+          }
+          if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+            setHeadr('')
+            setTitle('')
+            setDesc('')
+            loadData()
           } else if (info.file.status === 'error') {
             message.error(`${info.file.name} file upload failed.`);
           }
@@ -76,7 +82,7 @@ export default function Home() {
     };
     const loadData=()=>{
         setLoading(true)
-        fetch('https://modibbo-adama.herokuapp.com/admin/get-home-event')
+        fetch('https://new-modibbo-adama.herokuapp.com/admin/get-home-event')
         .then(res => {
             res.json()
                 .then(data => {
@@ -110,8 +116,7 @@ export default function Home() {
         
         <div className='mainCont'>
         <div className='heading'>
-        <h1>Current Home Data</h1>
-        <h1>Edit Data</h1>
+        <h1>Main Events</h1>
         </div>
         
         <div className='homeCont'>
@@ -131,20 +136,48 @@ export default function Home() {
                 {rec.image?<img src={rec.image}/>:<h4>No Image Data</h4>}
                 <h3>Sub Heading</h3>
                <Input style={{
-                   marginTop:10,
+                   marginTop:5,
                    width:'70%'
                }} placeholder={`${rec.subHeader}`}/>
                 <h4>Title</h4>
                 <Input style={{
-                   marginTop:10,
+                   marginTop:5,
                    width:'70%'
                }} placeholder={`${rec.header}`}/>
                 <p>Description</p>
                 <Input style={{
-                   marginTop:10,
+                   marginTop:5,
                    width:'70%',
                    marginBottom:10
                }} placeholder={`${rec.description}`}/>
+               <DeleteOutlined onClick={()=>{
+                 const confirm=window.confirm('Are You Sure?')
+                 if (confirm) {
+               
+                  fetch(`https://new-modibbo-adama.herokuapp.com/admin/remove-event?eventName=mainEvents&eventId=${rec.evntId}`,{
+                    method:'PUT',
+                    headers:{
+                      "Content-Type":'application/json'
+                    },
+                  
+                  })
+                  .then(res=>{
+                      res.json()
+                      .then(data=>{
+                         message.success('deleted Succesfuly')
+                         loadData()
+                      })
+                  })
+
+
+
+                  
+                 }
+               }} style={{
+                 color:'red',
+                 fontSize:20,
+                 cursor:'pointer'
+               }}/>
             </div>
              )):(
                  <div>
@@ -184,10 +217,91 @@ export default function Home() {
            setDesc(txt.target.value)
        }} style={{marginTop:20,marginBottom:20}} placeholder='Enter Description'/>
 
+       <Button onClick={()=>{
+          const myObj={
+              evnt:{
+                  header:title,
+                  subHeader:header,
+                  description:desc
+              },
+              homeEventType:'mainEvents'
+          }
+          fetch('https://new-modibbo-adama.herokuapp.com/admin/create-home-event',{
+              method:'PUT',
+              headers:{
+                "Content-Type":'application/json'
+              },
+              body:JSON.stringify(myObj)
+            })
+            .then(res=>{
+                res.json()
+                .then(data=>{
+                   setUploadOpt(data.newlyEvent.evntId)
+                   console.log(data.newlyEvent.evntId,"+++++")
+                   message.success('doneeeeeee')
+                })
+            })
+      
+       }} color='primary'>Upate Text</Button>
+
 <Upload  {...props}>
     <Button icon={<UploadOutlined />}>Click to Upload an Image</Button>
 </Upload>
       </Modal>
+
+
+<h1>Vc's Message</h1>
+<div className='vcMessage'>
+  {
+     !isloading&&
+     homeData.length>0&&
+     homeData[0].vc.description!==''?(
+       <>
+  <img src={homeData[0].vc.image}/>
+  <Upload  {...props2}>
+    <Button icon={<UploadOutlined />}>Click to Change Vc's Image</Button>
+</Upload>
+<h4>Vc's Name</h4>
+<Input onChange={(txt)=>{
+  setHeadr(txt.target.value)
+}} style={{width:'70%'}} placeholder={homeData[0].vc.header}/>
+<h4>Vc's Message</h4>
+<TextArea onChange={(txt)=>{
+  setDesc(txt.target.value)
+}} style={{width:'70%'}} placeholder={homeData[0].vc.description}/>
+<Button onClick={()=>{
+  const myObj={
+    eventId:homeData[0].vc.evntId,
+    eventName:'vc',
+    evnt:{
+        header:header,
+        subHeader:'',
+        description:desc
+    }
+}
+console.log(myObj)
+// fetch('https://new-modibbo-adama.herokuapp.com/admin/edit-homepage-event',{
+//     method:'PUT',
+//     headers:{
+//       "Content-Type":'application/json'
+//     },
+//     body:JSON.stringify(myObj)
+//   })
+//   .then(res=>{
+//       res.json()
+//       .then(data=>{
+//          loadData()
+//          message.success('doneeeeeee')
+//       })
+//   })
+}} style={{marginTop:10,marginBottom:10}} type='primary'>Save Edited Text</Button>
+       </>
+     ):(
+       <h1>none</h1>
+     )
+  }
+
+</div>
 
         </div>
     )
